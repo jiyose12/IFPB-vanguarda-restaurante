fba56ff3bda3a350bb2840823304eba0b7c34a15
jiyose <jiyose@outlook.com>
2019-08-07 13:43:47 -0300

    metodo removeOne OK

--- app/Http/Controllers/ItensPedidosController.php
+++ app/Http/Controllers/ItensPedidosController.php
@@ -1,4 +1,6 @@
-<?php namespace App\Http\Controllers;
+<?php
+
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -9,45 +11,51 @@ use App\ItensPedido;
 
 class ItensPedidosController extends Controller
 {
-    public static function addToCart(Int $iditem, Int $idpedido){
+    public static function addToCart(Int $iditem, Int $idpedido)
+    {
         $item = Itens::find($iditem);
         $pedido = Pedido::find($idpedido);
 
         $pedido->itens()->attach($item);
     }
 
-    public static function removeFromCart(Int $iditem, Int $idpedido, $all = false){
+    public static function removeFromCart(Int $iditem, Int $idpedido, $all = false)
+    {
 
-        if($all) {
-        $itenspedidos = ItensPedido::where('itens_id', $iditem)
-        ->where('pedido_id', $idpedido)
-        ->where('dtremoved', NULL)
-        ->update(['dtremoved' => NOW()]);
- 
+        if ($all) {
+            $itenspedidos = ItensPedido::where('itens_id', $iditem)
+                ->where('pedido_id', $idpedido)
+                ->where('dtremoved', NULL)
+                ->update(['dtremoved' => NOW()]);
+
+            return $itenspedidos;
         } else {
-        $itenspedidos = ItensPedido::where('itens_id', $iditem)
-        ->where('pedido_id', $idpedido)
-        ->where('dtremoved', NULL)
-        ->limit(1)
-        ->update(['dtremoved' => NOW()]);
-        // $itenspedidos = DB::update("update itens_pedidos set dtremoved = NOW() where itens_id = ? and pedido_id = ? and dtremoved is NULL limit 1", [$iditem, $idpedido]);
+            $itempedido = ItensPedido::where('itens_id', $iditem)
+                ->where('pedido_id', $idpedido)
+                ->where('dtremoved', NULL)
+                ->first();
+
+            $itempedido->update(['dtremoved' => NOW()]);
+            // $itenspedidos = DB::update("update itens_pedidos set dtremoved = NOW() where itens_id = ? and pedido_id = ? and dtremoved is NULL limit 1", [$iditem, $idpedido]);
+            return $itempedido;
         }
-        return $itenspedidos;
     }
 
-    public function add(Request $request){
+    public function add(Request $request)
+    {
 
         $pedido = PedidosController::getFromSession($request);
 
         ItensPedidosController::addToCart($request->iditem, $pedido->id);
-        
+
         return  redirect()->route('cart');
     }
 
-    public function removeOne(Request $request){
+    public function removeOne(Request $request)
+    {
 
         $pedido = PedidosController::getFromSession($request);
-        
+
         $itenspedidos = ItensPedidosController::removeFromCart($request->iditem, $pedido->id);
 
         $request->session()->flash(
@@ -57,20 +65,22 @@ class ItensPedidosController extends Controller
         return  redirect()->route('cart');
     }
 
-    public function removeAll(Request $request){
+    public function removeAll(Request $request)
+    {
 
         $pedido = PedidosController::getFromSession($request);
 
         $itenspedidos = ItensPedidosController::removeFromCart($request->iditem, $pedido->id, true);
-        
+
         $request->session()->flash(
             'mensagem',
-            "Itens removido com sucesso"
+            "Itens removidos com sucesso"
         );
         return  redirect()->route('cart');
     }
 
-    public function listCart(Request $request){
+    public function listCart(Request $request)
+    {
 
         $pedido = PedidosController::getFromSession($request);
         $mensagem = $request->session()->get('mensagem');
@@ -79,14 +89,19 @@ class ItensPedidosController extends Controller
             ->join('itens', 'itens_pedidos.itens_id', '=', 'itens.id')
             ->where('itens_pedidos.pedido_id', $pedido->id)
             ->whereNull('itens_pedidos.dtremoved')
-            ->select('itens.id', 'itens.nome', 'itens.categoria', 'itens.preco_bruto', 'img_itens', 
-            DB::raw('count(*) as nrqtd'),
-            DB::raw('SUM(itens.preco_bruto) as vltotal'))
+            ->select(
+                'itens.id',
+                'itens.nome',
+                'itens.categoria',
+                'itens.preco_bruto',
+                'img_itens',
+                DB::raw('count(*) as nrqtd'),
+                DB::raw('SUM(itens.preco_bruto) as vltotal')
+            )
             ->groupBy('itens.id', 'itens.nome', 'itens.categoria', 'itens.preco_bruto', 'img_itens')
             ->orderBy('itens.nome')
             ->get();
 
         return view('pedidos.cart', compact('pedido', 'mensagem', 'cart'));
     }
-
 }
